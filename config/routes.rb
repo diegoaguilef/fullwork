6c044f91e3288f28381456a9cbc7380722325412
Diego Aguilef <diego.aguilef@gmail.com>
2020-06-29 00:16:45 -0400

    adds devise sesssions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
-  get 'home/index'
+
+  root to: redirect('/home'), as: :landing_page
+  get '/home', to: 'home#index'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations'
+  }
+
+  authenticated do
+    root 'dashboard#index', as: :user_root
+  end
+
+  authenticate do
+    get 'dashboard/index'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
